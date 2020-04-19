7e676cf3a6995b07c8035fd6a0dab94d94506e7e
Robert Bovee <Rjbovee112@gmail.com>
2020-04-19 10:31:50 -0600

    added all css and html from project into react

---
+++ src/pages/projects/projects.js
@@ -0,0 +1,35 @@
+<div class="container">
+    <div class="row">
+
+
+        <div class=" col-md-3"><img id="dream" src="./assets/DreamD.jpg">
+            <div class="centered"><a href="https://ruffmantom.github.io/Group-project/" target="_blank">
+                <font size="6"><b>DREAM</b></font>
+            </a></div>
+    </div>
+
+
+            <div class=" col-md-3"><img id="note" src="./assets/NoteTaker.jpg">
+                <div class="centered"><a href="https://blooming-ridge-22773.herokuapp.com/" target="_blank">
+                    <font size="6"><b>NOTE</b></font>
+                </a></div>
+    </div>
+
+
+                <div class=" col-md-3"><img id="weather" src="./assets/weatherAPI.jpg">
+                    <div class="centered"><a href="https://rjbovee112.github.io/Weather-APIHW/" target="_blank">
+                        <font size="6"><b>WEATHER</b></font>
+                    </a></div>
+    </div>
+
+
+                    <div class=" col-md-3"><img id="generator" src="./assets/generator.jpg">
+                        <div class="centered"><a href="https://github.com/Rjbovee112/Developer-Profile-Generator--09"
+                            target="_blank">
+                            <font size="6"><b>PROFILE</b></font>
+                        </a></div>
+    </div>
+
+
+                    </div>
+                </div>
