e35f269f7ee632bbcfc60b03561eb16e3ec314d8
Robert Bovee <Rjbovee112@gmail.com>
2020-04-20 18:15:51 -0600

    working on adjusting the footer and icons

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -6,15 +6,15 @@ import './footersyle.css';
 
 function Footer() {
     return (
-        <div class="col-md-12 foot-icons">
-            <a class="icons" target="_blank" href="https://github.com/Rjbovee112"><img style={{ height: '50%' }}
+        <div className="col-md-12 foot-icons">
+            <a className="icons" target="_blank" href="https://github.com/Rjbovee112"><img style={{ height: '40px' }}
                 src="assets/kisspng-github-computer-icons-repository-arch-linux-icon-5b4c339a5ea7f8.7251585315317206023877.png" /></a>
 
-            <a class="icons" target="_blank" href="https://www.linkedin.com/in/robert-bovee-a5b0a3191/"><img
-                style={{ height: '100%' }} src="assets/linkedin-512.png" /></a>
-            <a class="icons" target="_blank" href="index.html"><img style={{ height: '100 %' }} src="assets/logo-one.png" /></a>
+            <a className="icons" target="_blank" href="https://www.linkedin.com/in/robert-bovee-a5b0a3191/"><img style={{ height: '40px' }}
+                src="assets/linkedin-512.png" /></a>
+            <a className="icons" target="_blank" href="index.html"><img style={{ height: '40px' }} src="assets/logo-one.png" /></a>
 
-            <a class="icons" target="_blank" href="https://www.kidney.org/"><img style={{ height: '100 %' }}
+            <a className="icons" target="_blank" href="https://www.kidney.org/"><img style={{ height: '40px' }}
                 src="assets/kindpng_2270139.png" /></a>
         </div>
     );
